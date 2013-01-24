1ab51183a80ada757a7cbec98a27b0c1d20554a9
Tarick Bedeir <tarick@bedeir.com>
2013-01-24 00:01:02 +0000

    add build-debian.sh

--- scripts/build-ubuntu.sh
+++ scripts/build-ubuntu.sh
@@ -12,7 +12,7 @@ function die()
 
 [[ ! -z "$_DIST" ]] || die "Usage: $0 <dist-tarball>"
 [[ -f "$_DIST" ]] || die "Can't find dist package [$_DIST]."
-[[ -d debian ]] || die "Run me from the top-level packaging directory."
+[[ -d ubuntu ]] || die "Run me from the top-level packaging directory."
 [[ "$_DIST" == "${_DIST%%.tar.gz}.tar.gz" ]] || die "Tarball name needs to end in .tar.gz."
 
 pushd $(dirname $_DIST) >/dev/null || die "Can't enter dist dir."
@@ -21,9 +21,9 @@ popd >/dev/null
 
 mkdir -p output || die "Can't create output dir."
 
-rm -rf build/deb-build
-mkdir -p build/deb-build || die "Can't create build dir."
-cd build/deb-build || die "Can't enter build dir."
+rm -rf build/ub-build
+mkdir -p build/ub-build || die "Can't create build dir."
+cd build/ub-build || die "Can't enter build dir."
 
 TAR_NAME=$(basename $_DIST)
 TAR_NAME=${TAR_NAME%%.tar.gz}
@@ -37,6 +37,17 @@ cd debian || die "Can't enter debian"
 
 find . -type d -name .svn | xargs rm -rf
 
+rm control || die "Can't remove debian control file."
+cp $PKG_DIR/ubuntu/control . || die "Can't copy Ubuntu control file."
+
+mv changelog changelog.debian || die "Can't rename debian changelog."
+
+cp $PKG_DIR/ubuntu/changelog . || die "Can't copy Ubuntu changelog."
+
+echo >> changelog
+cat changelog.debian >> changelog || die "Can't update changelog."
+rm changelog.debian || die "Can't remove debian changelog."
+
 echo >> changelog
 cat ../ChangeLog >> changelog || die "Can't update changelog."
 
@@ -44,11 +55,11 @@ debuild || die "debuild failed."
 
 cd $PKG_DIR
 
-mv build/deb-build/*.tar.gz output/
-mv build/deb-build/*.dsc output/
-mv build/deb-build/*.build output/
-mv build/deb-build/*.changes output/
-mv build/deb-build/*.deb output/ || die "Can't move .deb files."
+mv build/ub-build/*.tar.gz output/
+mv build/ub-build/*.dsc output/
+mv build/ub-build/*.build output/
+mv build/ub-build/*.changes output/
+mv build/ub-build/*.deb output/ || die "Can't move .deb files."
 
-rm -rf build/deb-build
+rm -rf build/ub-build
 rmdir build 2>/dev/null
